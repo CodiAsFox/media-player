1507ceb0c7c3c25b226e2225b596155223c0ed7f
Taylor Ramirez <rami0101@algonquinlive.com>
2023-01-26 14:02:50 -0500

    Started phase two

--- js/app.js
+++ js/app.js
@@ -1,5 +1,69 @@
 const log = console.log;
-import MEDIA from "./media.js";
+// const warn = console.warn;
+// const error = console.error;
 
-log("Hello... It's me");
-log(MEDIA);
+import MEDIA1 from "./media.js";
+
+// To assign event
+const errorHandler = new Event("error");
+
+// To trigger the event Listener
+window.addEventListener("error", (msg) => {
+  log(msg);
+  console.log("The start event was triggered");
+});
+
+const APP = {
+  audio: new Audio(), //the Audio Element that will play every track
+  currentTrack: 0, //the integer representing the index in the MEDIA array
+  init: () => {
+    //called when DOMContentLoaded is triggered
+    log("init");
+    APP.addListeners();
+
+    try {
+      if (MEDIA.length > 0) APP.buildPlaylist();
+      else APP.error("bbbbb");
+    } catch {
+      log("error");
+      //   dispatchEvent("error");
+      //   log(APP.error("dddd"));
+      APP.error("dddd");
+    }
+  },
+  addListeners: () => {
+    //add event listeners for interface elements
+    //add event listeners for APP.audio
+    // APP.error = document.createEvent("error");
+
+    // To trigger the Event
+    APP.error = (msg) => {
+      document.dispatchEvent(errorHandler, msg);
+    };
+  },
+  buildPlaylist: () => {
+    log(MEDIA);
+  },
+  loadCurrentTrack: () => {
+    //use the currentTrack value to set the src of the APP.audio element
+  },
+  play: () => {
+    //start the track loaded into APP.audio playing
+  },
+  pause: () => {
+    //pause the track loaded into APP.audio playing
+  },
+  convertTimeDisplay: (seconds) => {
+    //convert the seconds parameter to `00:00` style display
+  },
+};
+
+// function errorHandler(ev) {
+//   //do what you want when the audio/video track cannot load
+//   // ev.type = 'error'
+//   console.error("aaaaa", ev);
+// }
+
+// window.addEventListener("error", errorHandler);
+
+document.addEventListener("DOMContentLoaded", APP.init);
