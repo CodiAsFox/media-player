06603be6b852537e807783d0515e75dc7a494ab5
Taylor Ramirez <rami0101@algonquinlive.com>
2023-03-14 19:44:25 -0400

    Implement Steve's feedback.

--- js/app.js
+++ js/app.js
@@ -4,61 +4,33 @@ const APP = {
   audio: new Audio(),
   currentTrack: 0,
   appName: "madTunes",
-  init: () => {
+  init: async () => {
     APP.playerView = document.getElementById("playerView");
     APP.playlist = document.getElementById("playlist");
     APP.playerControls = document.getElementById("player-controls");
     APP.totalTime = document.querySelector(".times .total-time");
     APP.currentTime = document.querySelector(".times .current-time");
     APP.progress = document.querySelector(".progress");
-    APP.btnPlay = document.getElementById("btnPlay");
-    APP.btnPause = document.getElementById("btnPause");
+    APP.btnPlayPause = document.getElementById("btnPlayPause");
     APP.btnShuffle = document.getElementById("btnShuffle");
 
     try {
-      APP.buildPlaylist()
-        .then((setlist) => {
-          if (setlist.length > 0) {
-            APP.setlist = parseHTML(setlist);
-            return true;
-          } else {
-            throw Error(`The playlist couldn't be generated`);
-          }
-        })
-        .then(async () => {
-          for (const track of MEDIA) {
-            await APP.getTrackDuration(track.track)
-              .then((timeSeconds) => {
-                const timeMinutes = APP.convertTimeDisplay(timeSeconds);
-                const totalTime = `<time datetime="${timeSeconds}" class="total-time">${timeMinutes}</time>`;
+      const setlist = APP.buildPlaylist();
+      if (setlist.length > 0) {
+        APP.setlist = parseHTML(setlist);
 
-                APP.setlist.querySelector(
-                  `[data-track="${track.track}"]`
-                ).dataset.runtime = timeMinutes;
-
-                return totalTime;
-              })
-              .then((totalTime) => {
-                APP.setlist.querySelector(
-                  `[data-track="${track.track}"] .track--time`
-                ).innerHTML = totalTime;
-              });
-          }
-        })
-        .then(() => {
-          APP.playlist.innerHTML = APP.setlist.innerHTML;
-          APP.btnShuffle.disabled = false;
-        })
-        .then(() => {
-          APP.addListeners();
-          APP.currentTrack = {
-            track: MEDIA[0].track,
-            title: MEDIA[0].title,
-            large: MEDIA[0].large,
-            banner: MEDIA[0].banner,
-          };
-          APP.loadCurrentTrack();
+        APP.playlist.innerHTML = APP.setlist.innerHTML;
+        APP.btnShuffle.disabled = false;
+        APP.addListeners();
+        MEDIA.forEach((track, index) => {
+          APP.getTrackDuration(track, index);
         });
+
+        APP.currentTrack = 0;
+        APP.loadCurrentTrack();
+      } else {
+        throw Error(`The playlist couldn't be generated`);
+      }
     } catch {
       throw new Error("The MEDIA.js file couldn't be found.");
     }
@@ -66,42 +38,35 @@ const APP = {
   addListeners: () => {
     window.addEventListener("error", errorHandler);
     APP.audio.addEventListener("error", audioErrorHandler);
-    APP.audio.addEventListener("loadeddata", (ev) => {
-      let duration = APP.audio.duration;
+    APP.audio.addEventListener("loadeddata", APP.loadDuration);
+    APP.audio.addEventListener("timeupdate", APP.updateTimestamp);
+    APP.playlist.addEventListener("click", APP.playSelected);
+    APP.btnPlayPause.addEventListener("click", APP.playPauseBtn);
+  },
+  loadDuration: () => {
+    let duration = APP.audio.duration;
+    APP.totalTime.innerHTML = APP.convertTimeDisplay(duration);
+    APP.totalTime.setAttribute("datetime", duration);
+  },
+  updateTimestamp: () => {
+    const currentTime = APP.audio.currentTime;
+    const duration = APP.audio.duration;
+    if (duration && currentTime) APP.playProgress(duration, currentTime);
+  },
+  playSelected: (ev) => {
+    const playing = APP.playlist.querySelector(".playing");
+    if (playing) playing.classList.remove("playing");
 
-      APP.totalTime.innerHTML = APP.convertTimeDisplay(duration);
-      APP.totalTime.setAttribute("datetime", duration);
-    });
+    const track = ev.target.closest(".track");
+    APP.currentTrack = track.dataset.track;
+    APP.loadCurrentTrack();
+    APP.play();
 
-    APP.audio.addEventListener("timeupdate", (ev) => {
-      const currentTime = APP.audio.currentTime;
-      const duration = APP.audio.duration;
-      if (duration && currentTime) APP.playProgress(duration, currentTime);
-    });
-
-    const tracks = APP.playlist.querySelectorAll("li");
-
-    tracks.forEach((track) => {
-      track.addEventListener("click", (ev) => {
-        const playing = APP.playlist.querySelector(".playing");
-
-        if (playing) playing.classList.remove("playing");
-
-        APP.currentTrack = track.dataset;
-        APP.loadCurrentTrack();
-        APP.play();
-      });
-    });
-
-    APP.btnPlay.addEventListener("click", APP.play);
-    APP.btnPause.addEventListener("click", APP.pause);
+    track.classList.contains("playing");
   },
   buildPlaylist: () => {
-    const builder = new Promise((resolve, reject) => {
-      let playlist = MEDIA.map((obj) => {
-        const trackID = obj.track;
-
-        const listItem = `<li class="track" data-track="${trackID}" data-title="${obj.title}" data-large="${obj.large}" data-banner="${obj.banner}" tabindex="0">
+    const playlist = MEDIA.map((obj, index) => {
+      const listItem = `<li class="track" data-track="${index}" tabindex="0">
                     <div class="track--thumb">
                         <img src="./img/${obj.thumbnail}" alt="${obj.title} art">
                     </div>
@@ -117,68 +82,70 @@ const APP = {
                         </div>
                     </div>
                 </li>`;
-        return listItem;
-      }).join("");
-
-      if (playlist) {
-        resolve(playlist);
-      } else {
-        setTimeout(
-          () =>
-            reject(
-              new Error("Something went wrong while building the playlist")
-            ),
-          1000
-        );
-      }
-    });
-
-    return builder;
+      return listItem;
+    }).join("");
+    if (playlist) {
+      return playlist;
+    } else {
+      throw new Error("Something went wrong while building the playlist");
+    }
   },
-  getTrackDuration: (track) => {
-    const duration = new Promise((resolve, reject) => {
-      let trackObj = new Audio(`./media/${track}`);
-      trackObj.addEventListener("loadeddata", () => {
-        const duration = trackObj.duration;
+  getTrackDuration: ({ track }, index) => {
+    const trackObj = new Audio(`./media/${track}`);
+    trackObj.addEventListener("durationchange", (ev) => {
+      const duration = ev.target.duration;
 
-        if (duration) {
-          resolve(duration);
-        } else {
-          reject(
-            new Error("Something went wrong while getting the track duration.")
-          );
-        }
-      });
+      const timeMinutes = APP.convertTimeDisplay(duration);
+      const totalTime = `<time datetime="${duration}" class="total-time">${timeMinutes}</time>`;
+
+      APP.playlist.querySelector(`[data-track="${index}"]`).dataset.runtime =
+        timeMinutes;
+      APP.playlist.querySelector(
+        `[data-track="${index}"] .track--time`
+      ).innerHTML = totalTime;
     });
-
-    return duration;
   },
   loadCurrentTrack: () => {
-    APP.audio.src = `./media/${APP.currentTrack.track}`;
-    document.title = `${APP.appName} | Playing ${APP.currentTrack.title} by ${APP.currentTrack.artist}`;
-    const albumArt = `<img src="./img/${APP.currentTrack.large}" alt="${APP.currentTrack.title} album">`;
+    let track = MEDIA[APP.currentTrack];
+
+    APP.audio.src = `./media/${track.track}`;
+    document.title = `${APP.appName} | Playing ${track.title} by ${track.artist}`;
+    const albumArt = `<img src="./img/${track.large}" alt="${track.title} album">`;
     APP.playerView.querySelector(".img--container").innerHTML = albumArt;
 
-    APP.playerView.style.backgroundImage = `url('./img/${APP.currentTrack.banner}')`;
+    APP.playerView.style.backgroundImage = `url('./img/${track.banner}')`;
     APP.playlist
-      .querySelector(`[data-track="${APP.currentTrack.track}"]`)
+      .querySelector(`[data-track="${APP.currentTrack}"]`)
       .classList.add("playing");
 
     APP.playerControls.classList.add("active");
-    APP.btnPlay.disabled = false;
-    APP.btnPause.disabled = true;
+    APP.btnPlayPause.disabled = false;
+  },
+  playPauseBtn: (ev) => {
+    const btn = ev.target.closest(".btn");
+    const action = btn.dataset.action;
+    switch (action) {
+      case "play":
+        APP.play();
+        break;
+      case "pause":
+        APP.pause();
+        break;
+    }
   },
   play: () => {
     APP.audio.play();
     APP.playerView.classList.add("playing");
-    APP.btnPlay.disabled = true;
-    APP.btnPause.disabled = false;
+    APP.btnPlayPause.title = "Pause";
+    APP.btnPlayPause.dataset.action = "pause";
+    APP.btnPlayPause.querySelector("i").innerHTML = "pause";
   },
   pause: () => {
     APP.audio.pause();
     APP.playerView.classList.remove("playing");
-    APP.btnPlay.disabled = false;
-    APP.btnPause.disabled = true;
+    APP.btnPlayPause.title = "Play";
+    APP.btnPlayPause.dataset.action = "play";
+    APP.btnPlayPause.querySelector("i").innerHTML = "play_arrow";
   },
   convertTimeDisplay: (seconds) => {
     let minutes = Math.floor(seconds / 60);
@@ -190,7 +157,6 @@ const APP = {
   playProgress: (duration, currentTime) => {
     APP.currentTime.innerHTML = APP.convertTimeDisplay(currentTime);
     APP.currentTime.setAttribute("datetime", currentTime);
-
     const progress = (currentTime / duration) * 100;
     APP.progress.style.width = `${progress.toFixed(2)}%`;
   },
